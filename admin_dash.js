eff11b396f37e9bef4f07719d49518f142f8ede5
biswarup <bis.banerjee.bb@gmail.com>
2017-08-12 00:32:58 +0530

    added UI components

--- admin_dash.js
+++ admin_dash.js
@@ -1,25 +1,8 @@
 $(document).ready(function(){
   $('#title_qit').addClass('animated fadeInDown');
   $('#animate_row').addClass('animated fadeInUp');
-  // Initialize collapse button
  $(".button-collapse").sideNav();
- // Initialize collapsible (uncomment the line below if you use the dropdown variation)
- //$('.collapsible').collapsible();
-    // $("#chatButton").click(function(){
-    //     $("#hide").toggle();
-    // });
-    //
-    // $("button").click(function(){
-    //       $("p").toggle();
-    //   });\
 });
-
-$(document).ready(function() {
-$("#chatButton").click(function() {
-$("#hide").hide();
-});
-});
-
 
 function toggle() {
     var x = document.getElementById('hide');
@@ -29,7 +12,5 @@ function toggle() {
         y.style.backgroundColor =  '#847ae2';
     } else {
         x.style.display = 'none';
-
-
     }
 }
