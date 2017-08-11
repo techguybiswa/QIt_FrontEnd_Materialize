3111ae8483e798f51abad653046830f9fd823454
biswarup <bis.banerjee.bb@gmail.com>
2017-08-11 21:21:04 +0530

    added fixed button

--- admin_dash.js
+++ admin_dash.js
@@ -5,5 +5,31 @@ $(document).ready(function(){
  $(".button-collapse").sideNav();
  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
  //$('.collapsible').collapsible();
-
+    // $("#chatButton").click(function(){
+    //     $("#hide").toggle();
+    // });
+    //
+    // $("button").click(function(){
+    //       $("p").toggle();
+    //   });\
 });
+
+$(document).ready(function() {
+$("#chatButton").click(function() {
+$("#hide").hide();
+});
+});
+
+
+function toggle() {
+    var x = document.getElementById('hide');
+    var y = document.getElementById('chatButton');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+        y.style.backgroundColor =  '#847ae2';
+    } else {
+        x.style.display = 'none';
+
+
+    }
+}
