ee7e976a522cb1b62864033bf5bc0b1a1e1b872d
biswarup <bis.banerjee.bb@gmail.com>
2017-08-11 19:42:44 +0530

    front end of QIt

---
+++ admin_dash.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('#title_qit').addClass('animated fadeInDown');
+  $('#animate_row').addClass('animated fadeInUp');
+  // Initialize collapse button
+ $(".button-collapse").sideNav();
+ // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+ //$('.collapsible').collapsible();
+
+});
