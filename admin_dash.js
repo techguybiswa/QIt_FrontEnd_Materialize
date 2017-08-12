98a09c102d8bba4ddc199ff96d6bfa2fce2cdebc
biswarup <bis.banerjee.bb@gmail.com>
2017-08-12 18:38:47 +0530

    Siddhartha UI edits

--- admin_dash.js
+++ admin_dash.js
@@ -2,15 +2,20 @@ $(document).ready(function(){
   $('#title_qit').addClass('animated fadeInDown');
   $('#animate_row').addClass('animated fadeInUp');
   $('#title2').addClass('animated slideInLeft');
+  $('#chatButton').addClass('animated rotateInDownLeft');
+
+
 
 });
 
 function toggle() {
     var x = document.getElementById('hide');
     var y = document.getElementById('chatButton');
+    var z = document.getElementById('hideMe');
     if (x.style.display == 'none') {
         x.style.display = 'block';
-        y.style.backgroundColor =  '#847ae2';
+        y.style.backgroundColor =  '#87b5ff';
+        // z.style.color:
     } else {
         x.style.display = 'none';
     }
