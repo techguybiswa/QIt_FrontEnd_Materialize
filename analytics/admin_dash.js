907310ba5d17fb1d46b6c7cc4f33919b45a4e4d4
biswarup <bis.banerjee.bb@gmail.com>
2017-08-23 20:04:57 +0530

    minor edits

--- analytics/admin_dash.js
+++ analytics/admin_dash.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
   $('#animate_row').addClass('animated fadeInUp');
   $('#title2').addClass('animated slideInLeft');
   $('#chatButton').addClass('animated rotateInDownRight');
+
   // $('#chart_div').addClass('animated fadeInUp');
   // $('#chart_div_2').addClass('animated fadeInUp stayHidden');
   // $('#chart_div_3').addClass('animated fadeInUp stayHidden');
@@ -25,44 +26,51 @@ function toggle() {
         x.style.display = 'none';
     }
 }
-function showHourly()
-{
-  document.getElementById('chart_div').style.display = 'block';
+// function showHourly()
+// {
+//   document.getElementById('chart_div').style.display = 'block';
+//
+//
+//   document.getElementById('chart_div_2').style.display = 'none';
+//   document.getElementById('chart_div_3').style.display = 'none';
+//   document.getElementById('top_x_div').style.display = 'none';
+//
+// }
+// function showDaily()
+// {
 
-
-  document.getElementById('chart_div_2').style.display = 'none';
-  document.getElementById('chart_div_3').style.display = 'none';
-  document.getElementById('top_x_div').style.display = 'none';
-
-}
-function showDaily()
-{
-
-  document.getElementById('chart_div_2').style.display = 'block';
-  
-  document.getElementById('chart_div_2').style.height = '';
-  document.getElementById('chart_div_2').style.width = 'block';
-
-  document.getElementById('chart_div').style.display = 'none';
-  document.getElementById('chart_div_3').style.display = 'none';
-  document.getElementById('top_x_div').style.display = 'none';
-
-
-
-}
-function showWeekly()
-{
-  document.getElementById('chart_div_3').style.display = 'block';
-  document.getElementById('chart_div').style.display = 'none';
-  document.getElementById('chart_div_2').style.display = 'none';
-  document.getElementById('top_x_div').style.display = 'none';
-}
-function showPeak()
-
-{
-  document.getElementById('top_x_div').style.display = 'block';
-  document.getElementById('chart_div_3').style.display = 'none';
-  document.getElementById('chart_div').style.display = 'none';
-  document.getElementById('chart_div_2').style.display = 'none';
-
-}
+  // document.getElementById('chart_div_2').style.display = 'block';
+  //
+  // document.getElementById('chart_div_2').style.height = '';
+  // document.getElementById('chart_div_2').style.width = 'block';
+  //
+  // document.getElementById('chart_div').style.display = 'none';
+  // document.getElementById('chart_div_3').style.display = 'none';
+  // document.getElementById('top_x_div').style.display = 'none';
+//   document.getElementById('chart_div_2').style.height = '500px';
+//   document.getElementById('chart_div_2').style.width = '1100px';
+//   document.getElementById('chart_div').style.height = '50px';
+//   document.getElementById('chart_div').style.width = '110px';
+//
+//
+//
+//
+//
+//
+// }
+// function showWeekly()
+// {
+//   document.getElementById('chart_div_3').style.display = 'block';
+//   document.getElementById('chart_div').style.display = 'none';
+//   document.getElementById('chart_div_2').style.display = 'none';
+//   document.getElementById('top_x_div').style.display = 'none';
+// }
+// function showPeak()
+//
+// {
+//   document.getElementById('top_x_div').style.display = 'block';
+//   document.getElementById('chart_div_3').style.display = 'none';
+//   document.getElementById('chart_div').style.display = 'none';
+//   document.getElementById('chart_div_2').style.display = 'none';
+//
+// }
