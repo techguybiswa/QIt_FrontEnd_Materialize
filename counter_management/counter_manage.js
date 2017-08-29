f86e48026b1909ba897c076c4442800be29c270f
biswarup <bis.banerjee.bb@gmail.com>
2017-08-29 19:08:14 +0530

    making UI components of counter managemnet

--- counter_management/counter_manage.js
+++ counter_management/counter_manage.js
@@ -8,70 +8,12 @@ $(document).ready(function(){
   // $('#chart_div').addClass('animated fadeInUp');
   // $('#chart_div_2').addClass('animated fadeInUp stayHidden');
   // $('#chart_div_3').addClass('animated fadeInUp stayHidden');
-  console.log("Hellooooooooooooooooooooooooooooooo");
 
 });
 
-function toggle() {
-    //console.log("CLicked");
-
-    var x = document.getElementById('hide');
-    var y = document.getElementById('chatButton');
-    //var z = document.getElementById('hideMe');
-    //console.log(x.style.display);
-    if (x.style.display == 'none') {
-        x.style.display = 'block';
-        y.style.backgroundColor =  '#575c82';
-        // z.style.color:
-    } else {
-        x.style.display = 'none';
-    }
-}
-// function showHourly()
-// {
-//   document.getElementById('chart_div').style.display = 'block';
-//
-//
-//   document.getElementById('chart_div_2').style.display = 'none';
-//   document.getElementById('chart_div_3').style.display = 'none';
-//   document.getElementById('top_x_div').style.display = 'none';
-//
-// }
-// function showDaily()
-// {
-
-  // document.getElementById('chart_div_2').style.display = 'block';
-  //
-  // document.getElementById('chart_div_2').style.height = '';
-  // document.getElementById('chart_div_2').style.width = 'block';
-  //
-  // document.getElementById('chart_div').style.display = 'none';
-  // document.getElementById('chart_div_3').style.display = 'none';
-  // document.getElementById('top_x_div').style.display = 'none';
-//   document.getElementById('chart_div_2').style.height = '500px';
-//   document.getElementById('chart_div_2').style.width = '1100px';
-//   document.getElementById('chart_div').style.height = '50px';
-//   document.getElementById('chart_div').style.width = '110px';
-//
-//
-//
-//
-//
-//
-// }
-// function showWeekly()
-// {
-//   document.getElementById('chart_div_3').style.display = 'block';
-//   document.getElementById('chart_div').style.display = 'none';
-//   document.getElementById('chart_div_2').style.display = 'none';
-//   document.getElementById('top_x_div').style.display = 'none';
-// }
-// function showPeak()
-//
-// {
-//   document.getElementById('top_x_div').style.display = 'block';
-//   document.getElementById('chart_div_3').style.display = 'none';
-//   document.getElementById('chart_div').style.display = 'none';
-//   document.getElementById('chart_div_2').style.display = 'none';
-//
-// }
+$('#addCounter').click(function(){
+    $('#counterDetails').css("display", "block");
+});
+$('#saveCounterDetails').click(function(){
+   $('#counterDetails').css("display", "none");
+});
